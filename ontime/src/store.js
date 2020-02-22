cb8224e4b82e64a19f735d754b668c6c8d5b237f
dbebli <beblidaniel@gmail.com>
2020-02-22 11:31:53 +0100

    remove redux dev tools from the store

--- ontime/src/store.js
+++ ontime/src/store.js
@@ -3,10 +3,10 @@ import {createStore} from "redux";
 import routeReducer from "./reducers";
 import {throttle} from 'lodash';
 
-
+//add to store for dev
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export const store = createStore(routeReducer, loadState(), reduxDevTools);
+export const store = createStore(routeReducer, loadState());
 
 
 //Wrapping the callback in a throttle ensures that the inner function that is passed in is not going to be called more often than the number of milliseconds that is specified
